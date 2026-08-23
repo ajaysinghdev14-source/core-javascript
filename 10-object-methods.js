ce9f1220a6c42a834729f1bd791c87be39396766
ajaysinghdev14-source <ajaysingh.dev14@gmail.com>
2026-08-23 12:41:42 +0530

    done with object methods

--- 10-object-methods.js
+++ 10-object-methods.js
@@ -1,8 +1,8 @@
 const artifact = {
-  name: "Obsidian Crown",
-  era: "Ancient",
-  value: 50000,
-  material: "volcanic glass",
+    name: "Obsidian Crown",
+    era: "Ancient",
+    value: 50000,
+    material: "volcanic glass",
 };
 
 const keys = Object.keys(artifact);
@@ -13,22 +13,22 @@ console.log(values); // array
 console.log(entries); // array of arrays
 
 for (const [key, value] of Object.entries(artifact)) {
-  console.log(`${key}: ${value}`);
+    console.log(`${key}: ${value}`);
 }
 
 const priceList = [
-  ["Obsidian Crown", 50000],
-  ["Ruby Crown", 30000],
-  ["value", 50000],
+    ["Obsidian Crown", 50000],
+    ["Ruby Crown", 30000],
+    ["value", 50000],
 ];
 
 const priceObject = Object.fromEntries(priceList);
 console.log(priceObject);
 
 const displayCase = {
-  artificat: "Obsidian",
-  location: "Hall A, Case 3",
-  locked: true,
+    artificat: "Obsidian",
+    location: "Hall A, Case 3",
+    locked: true,
 };
 
 Object.freeze(displayCase);
@@ -37,9 +37,35 @@ displayCase.test = "new value";
 console.log(displayCase);
 
 const catelogEntry = {
-  id: "ART-001",
-  description: "Ancient Crows",
-  verified: true,
+    id: "ART-001",
+    description: "Ancient Crows",
+    verified: true,
 };
 
 Object.seal(catelogEntry);
+
+const secureArtificats = {
+    name: "Ruby Pendant",
+};
+
+Object.defineProperty(secureArtificats, "catelogId", {
+    value: "SEC-9999",
+    writable: false,
+    enumerable: false,
+    configurable: false,
+});
+
+console.log(secureArtificats.catelogId);
+secureArtificats.catelogId = 'Hacked';
+console.log(secureArtificats.catelogId);
+
+for (const [key, value] of Object.entries(secureArtificats)) {
+    console.log(`${key}: ${value}`)
+}
+
+const desc = Object.getOwnPropertyDescriptor(secureArtificats, 'catelogId');
+console.log(desc);
+
+
+
+
