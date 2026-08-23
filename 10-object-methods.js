ca5dd20d06980e0d31572512a09620a34d4ad667
ajaysinghdev14-source <ajaysingh.dev14@gmail.com>
2026-08-23 12:46:42 +0530

    Added notes on different loop types and their use cases in object methods

--- 10-object-methods.js
+++ 10-object-methods.js
@@ -66,6 +66,15 @@ for (const [key, value] of Object.entries(secureArtificats)) {
 const desc = Object.getOwnPropertyDescriptor(secureArtificats, 'catelogId');
 console.log(desc);
 
+// Loop key points:
+//
+// 1. for loop - best optimize loop
+// 2. while loop - best for infinite loop
+// 3. do-while loop - best for loop with unknown condition
+// 4. for...in loop - best for looping over object properties and avoid on arrays
+// 5. for...of loop - best for looping over iterable objects
+// 6. map, foreach, filter, reduce
+
 
 
 
